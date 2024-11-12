b4779e7ae735f3e2b65d62e1b995d8bc43671098
Ayush-may <Ayushsharma014may@gmail.com>
2024-11-12 12:26:02 +0530

    added ssl file

--- routes/pool.js
+++ routes/pool.js
@@ -1,15 +1,36 @@
 const mysql = require('mysql')
+const fs = require('fs')
 
-var pool = mysql.createPool({
+// var pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     // password:'1234',
+//     database: process.env.DB_NAME,
+//     multipleStatements: true,
+//     connectionlimit: 100
+
+// })
+
+const pool = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    // password:'1234',
     database: process.env.DB_NAME,
     multipleStatements: true,
-    connectionlimit: 100
-
+    ssl: {
+        ca: fs.readFileSync('ca.pem')
+    }
 })
+
+pool.connect((err) => {
+    if (err) {
+        console.error('Connection failed: ', err.stack);
+        return;
+    }
+    console.log('Connected to Aiven MySQL as id ' + connection.threadId);
+});
 
 module.exports = pool
